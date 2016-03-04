d9bcee7485c9cbde8ade7cbb0f9d621cf7a0dbe0
Colin Hicks <colin.hicks@gmail.com>
2016-03-04 12:03:22 -0500

    support limit & offset in mock search reqs

--- test/crets/client_test.clj
+++ test/crets/client_test.clj
@@ -22,6 +22,15 @@
                      ["Class" "Residential"]
                      ["Query" "(Status=1)"]
                      ["Count" "2"]})))
+
+    (is (-> (sut/search-spec "Property" "Residential" "(Status=1)" :include-count? true)
+           sut/search-spec->request
+           utils/rets-search-request-parameters
+           set
+           (every? #{["SearchType" "Property"]
+                     ["Class" "Residential"]
+                     ["Query" "(Status=1)"]
+                     ["Count" "1"]})))
   
   (is (-> (sut/search-spec "Property" "Residential" "(Status=1)"
                             :limit 100 :offset 100)
@@ -32,8 +41,16 @@
                      ["Class" "Residential"]
                      ["Query" "(Status=1)"]
                      ["Limit" "100"]
-                     ["Offset" "100"]
-                     ["Count" "1"]}))))
+                     ["Offset" "100"]}))))
 
 
-(deftest search-results)
+(deftest mock-session-search-behavior
+  (is (= 1 (-> (utils/mock-session)
+               (sut/fetch-search {:limit 1})
+               .values
+               :rows
+               count)))
+
+  (is (not= (-> (utils/mock-session) (sut/fetch-search {:limit 1}) .values :rows first)
+            (-> (utils/mock-session) (sut/fetch-search {:limit 1 :offset 1}) .values :rows first))))
+
--- test/crets/test_utils.clj
+++ test/crets/test_utils.clj
@@ -1,8 +1,9 @@
 (ns crets.test-utils
   (:require [clojure.java.io :as io]
-            [clojure.string :as str])
-  (:import org.realtors.rets.common.metadata.Metadata)
-  (:import [org.realtors.rets.client RetsUtil GetMetadataResponse SearchResultHandler SearchResultImpl]))
+            [clojure.string :as str]
+            [crets.extensions :refer [IValues]])
+  (:import [org.realtors.rets.common.metadata Metadata]
+           [org.realtors.rets.client RetsUtil GetMetadataResponse SearchResultHandler SearchResultImpl]))
 
 (defn mock-metadata [path compact?]
   (with-open [stream (-> path io/resource io/input-stream)]
@@ -27,6 +28,14 @@
 (def compact-metadata (mock-metadata "test/compact-metadata.xml" true))
 (def search-result (mock-search-result "test/compact-search.xml"))
 
+(defn rets-search-request-parameters
+ "a vector of key-value vectors representing the request params"
+ [req]
+ (let [pairs (-> req .getHttpParameters (str/split #"&"))]
+   (->> pairs
+        (map #(str/split % #"="))
+        (map (fn [[k v]] [k (RetsUtil/urlDecode v)])))))
+
 (defprotocol IMockSession
   (login [this user-id pass])
   (getSessionId [this])
@@ -45,16 +54,24 @@
     compact-metadata)
 
   (search [_ req]
-    search-result))
+    ;; to support limit & offset, go fishing in the rets SearchRequest
+    ;; then reify IValues with the respectively limited :rows of the sample result
+    (let [params (->> req rets-search-request-parameters (into {}))
+          {:keys [limit offset] :or {limit Integer/MAX_VALUE offset 0}}
+          (->> (select-keys params ["Limit" "Offset"])
+                                (map (fn [[k v]] [(-> k str/lower-case keyword)
+                                                  (Integer/parseInt v)]))
+                                (into {}))]
+      (reify IValues
+        (values [_]
+          {:column-names (.getColumns search-result)
+           :total-count (.getCount search-result)
+           :rows (->> (.iterator search-result)
+                      iterator-seq
+                      (drop offset)
+                      (take limit))})))))
 
 (defn mock-session []
   (MockSession. (atom {})))
 
-(defn rets-search-request-parameters
- "a vector of key-value vectors representing the request params"
- [req]
- (let [pairs (-> req .getHttpParameters (str/split #"&"))]
-   (->> pairs
-        (map #(str/split % #"="))
-        (map (fn [[k v]] [k (RetsUtil/urlDecode v)])))))
 
